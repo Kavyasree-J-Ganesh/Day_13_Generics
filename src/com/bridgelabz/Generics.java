5ee28c171a3d64e7b950bd7687ffc54ad554858a
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-19 01:13:39 +0530

    [Kavyasree J Ganesh] UC2 Generics  problems

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -2,14 +2,14 @@ package com.bridgelabz;
 
 public class Generics {
     static void maximumOfVariables(Integer num1, Integer num2, Integer num3){
-        if(num1.compareTo(num2) == 1){
-            if(num1.compareTo(num3) == 1){
+        if(num1.compareTo(num2) > 0){
+            if(num1.compareTo(num3) > 0){
                 System.out.println( num1 + " is Maximum ");
             }else {
                 System.out.println( num3 + " is greater ");
             }
         }else{
-            if(num2.compareTo(num3) == 1){
+            if(num2.compareTo(num3) > 0){
                 System.out.println( num2 + " is maximum ");
             }else{
                 System.out.println( num3 + " is maximum ");
@@ -18,14 +18,14 @@ public class Generics {
     }
 
     static void maximumOfVariables(Float num1, Float num2, Float num3){
-        if(num1.compareTo(num2) == 1){
-            if(num1.compareTo(num3) == 1){
+        if(num1.compareTo(num2) > 0){
+            if(num1.compareTo(num3) > 0){
                 System.out.println( num1 + " is Maximum ");
             }else {
                 System.out.println( num3 + " is greater ");
             }
         }else{
-            if(num2.compareTo(num3) == 1){
+            if(num2.compareTo(num3) > 0){
                 System.out.println( num2 + " is maximum ");
             }else{
                 System.out.println( num3 + " is maximum ");
